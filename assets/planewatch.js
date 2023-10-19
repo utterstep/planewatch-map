9191e86c46c3aa9d08d723564aa8ca646fc8ec97
Vlad Stepanov <utterstep@hey.com>
2023-10-19 12:44:34 +0400

    Initial commit: basic map with WS updates & canvas rendering

---
+++ assets/planewatch.js
@@ -0,0 +1,92 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidXR0ZXJzdGVwIiwiYSI6ImNsbm40ZDUydzAyaHUyaXAxMmdwaTFianAifQ.1Is8_jQQFdwfFrANHEhGzA';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 8,
+    center: [44.7781, 41.7051]
+});
+
+const hashCode = s => s.split('').reduce((a,b) => (((a << 5) - a) + b.charCodeAt(0))|0, 0)
+const COLORS = ['#50BFE6','#EE34D2','#FD5B78','#FF00CC','#FF355E','#FF6037','#8FD400','#DA2C43','#6F2DA8','#FF6EFF','#FF3855','#FD3A4A','#FB4D46','#FA5B3D','#FFAA1D','#299617','#2243B6','#5DADEC','#5946B2','#9C51B6','#A83731','#AF6E4D','#FF5470','#FF7A00','#0048BA','#FF007C','#E936A7'];
+
+const pointsHistory = await fetch("/points_history").then(r => r.json());
+const pointsOverlay = document.getElementById("points-overlay");
+const pointsOverlayCtx = pointsOverlay.getContext("2d");
+
+const devicePixelRatio = window.devicePixelRatio || 1;
+
+function placePoint(mode_s, [lat, long], map, overlay, overlayCtx) {
+    const {x, y} = map.project([long, lat]);
+
+    if (x > 0 && y > 0 && x < overlay.width && y < overlay.height) {
+        overlayCtx.fillStyle = COLORS[hashCode(mode_s) % COLORS.length];
+        overlayCtx.fillRect((x - 1) * devicePixelRatio, (y - 1) * devicePixelRatio, 2 * devicePixelRatio, 2 * devicePixelRatio);
+    }
+}
+
+function drawFull(map, overlay, overlayCtx, points) {
+    overlayCtx.clearRect(0, 0, overlay.width, overlay.height);
+
+    const w = overlay.offsetWidth;
+    const h = overlay.offsetHeight
+
+    overlay.width = w * devicePixelRatio;
+    overlay.height = h * devicePixelRatio;
+
+    overlay.setAttribute("style", `width: ${w}px; height: ${h}px`);
+
+    for (const [mode_s, point] of points) {
+        placePoint(mode_s, point, map, overlay, overlayCtx);
+    }
+}
+
+drawFull(map, pointsOverlay, pointsOverlayCtx, pointsHistory);
+
+window.addEventListener('resize', () => {
+    drawFull(map, pointsOverlay, pointsOverlayCtx, pointsHistory);
+}, false);
+window.addEventListener("orientationchange", function() {
+    window.dispatchEvent(new Event("resize"));
+}, false);
+map.on('move', () => {
+    drawFull(map, pointsOverlay, pointsOverlayCtx, pointsHistory);
+});
+
+const wsProto = window.location.protocol === 'https:' ? 'wss:' : 'ws';
+
+const ws = new WebSocket(`${wsProto}//${window.location.host}/ws`);
+ws.addEventListener('message', (event) => {
+    function markerElement(isNew, mode_s) {
+        const pointsMarker = document.createElement("div");
+        pointsMarker.classList.add("marker");
+        if (isNew) {
+            pointsMarker.classList.add("marker-new");
+        }
+        pointsMarker.style.width = "4px";
+        pointsMarker.style.height = "4px";
+        pointsMarker.style.borderRadius = "4px";
+        pointsMarker.style.background = "#F22";
+
+        return pointsMarker;
+    }
+
+    const [mode_s, point] = JSON.parse(event.data);
+
+    if (isNaN(point[0]) || isNaN(point[1])) {
+        return;
+    }
+
+    pointsHistory.push([mode_s, point]);
+    placePoint(mode_s, point, map, pointsOverlay, pointsOverlayCtx);
+
+    const pointsMarker = markerElement(true);
+    const marker = new mapboxgl.Marker(pointsMarker);
+    marker.setLngLat({
+        lng: point[1],
+        lat: point[0],
+    }).addTo(map);
+
+    setTimeout(() => {
+        marker.remove();
+    }, 1000);
+});
---
+++ src/main.rs
@@ -0,0 +1,159 @@
+use std::{
+    collections::VecDeque,
+    error::Error,
+    net::{SocketAddr, TcpStream},
+    path::PathBuf,
+    sync::{Arc, Mutex},
+    thread::spawn,
+};
+
+use axum::{
+    extract::{
+        ws::{Message, WebSocket, WebSocketUpgrade},
+        ConnectInfo, State,
+    },
+    response::IntoResponse,
+    routing::get,
+    Json, Router,
+};
+use csv::ReaderBuilder;
+use smol_str::SmolStr;
+use tokio::sync::watch::{self, Receiver, Sender};
+use tower_http::{compression::CompressionLayer, services::ServeDir};
+
+#[derive(Clone)]
+pub struct AppState {
+    points_seen: Arc<Mutex<VecDeque<(SmolStr, (f32, f32))>>>,
+    sender: Arc<Sender<(SmolStr, (f32, f32))>>,
+}
+
+const POINTS_HISTORY_LIMIT: usize = 40000;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let points_seen = Arc::new(Mutex::new(VecDeque::with_capacity(POINTS_HISTORY_LIMIT)));
+    let (sender, _receiver) = watch::channel((SmolStr::default(), (f32::NAN, f32::NAN)));
+    let sender = Arc::new(sender);
+
+    let state = AppState {
+        points_seen: Arc::clone(&points_seen),
+        sender: Arc::clone(&sender),
+    };
+
+    let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
+
+    let app = Router::new()
+        .fallback_service(ServeDir::new(assets_dir))
+        .route("/points_history", get(points_history))
+        .route("/ws", get(ws_handler))
+        .layer(CompressionLayer::new())
+        .with_state(state);
+
+    spawn(move || {
+        println!("Created background task");
+
+        let stream = TcpStream::connect("127.0.0.1:30003").expect("failed to connect to source");
+        let mut reader = ReaderBuilder::new().flexible(true).from_reader(stream);
+
+        for record in reader.records() {
+            let record = record.expect("failed to parse source info");
+
+            let lat_long = record
+                .get(14)
+                .map(str::parse::<f32>)
+                .map(Result::ok)
+                .flatten()
+                .zip(
+                    record
+                        .get(15)
+                        .map(str::parse::<f32>)
+                        .map(Result::ok)
+                        .flatten(),
+                );
+
+            let mode_s = SmolStr::new(record.get(4).unwrap_or_default());
+
+            if let Some((lat, long)) = lat_long {
+                let mut points_seen = points_seen.lock().expect("points lock poisoned");
+
+                points_seen.push_back((mode_s.clone(), (lat, long)));
+
+                while points_seen.len() >= POINTS_HISTORY_LIMIT {
+                    points_seen.pop_front();
+                }
+
+                sender.send_replace((mode_s, (lat, long)));
+            }
+        }
+    });
+
+    axum::Server::bind(&"[::]:12345".parse().unwrap())
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .await?;
+
+    Ok(())
+}
+
+async fn points_history(State(state): State<AppState>) -> impl IntoResponse {
+    let points = state
+        .points_seen
+        .lock()
+        .expect("lock is poisoned")
+        .make_contiguous()
+        .to_vec();
+
+    Json::from(points)
+}
+
+/// The handler for the HTTP request (this gets called when the HTTP GET lands at the start
+/// of websocket negotiation). After this completes, the actual switching from HTTP to
+/// websocket protocol will occur.
+/// This is the last point where we can extract TCP/IP metadata such as IP address of the client
+/// as well as things from HTTP headers such as user-agent of the browser etc.
+async fn ws_handler(
+    ws: WebSocketUpgrade,
+    State(state): State<AppState>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    println!("{addr} connected.");
+    // finalize the upgrade process by returning upgrade callback.
+    // we can customize the callback by sending additional info such as address.
+    ws.on_upgrade(move |socket| handle_socket(socket, addr, state.sender.subscribe()))
+}
+
+/// Actual websocket statemachine (one will be spawned per connection)
+async fn handle_socket(
+    mut socket: WebSocket,
+    who: SocketAddr,
+    mut receiver: Receiver<(SmolStr, (f32, f32))>,
+) {
+    loop {
+        match receiver.changed().await {
+            Ok(()) => {
+                let (mode_s, (lat, long)) = receiver.borrow().clone();
+                println!("got change");
+
+                match socket
+                    .send(Message::Text(format!("[\"{mode_s}\",[{lat},{long}]]")))
+                    .await
+                {
+                    Ok(()) => {
+                        println!("update sent to {who}");
+                    }
+                    Err(e) => {
+                        eprintln!("Got error while sending: {e}");
+
+                        break;
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Got error while checking for updates: {e}");
+
+                break;
+            }
+        }
+    }
+
+    println!("Websocket context {who} destroyed");
+}
